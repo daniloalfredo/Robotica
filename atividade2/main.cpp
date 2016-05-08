ffa059b8f3f04b793acde62cde8bab314e906c76
aafu <aafu@cin.ufpe.br>
2016-05-08 20:16:27 -0300

    Versão inicial funcional do software da atividade 2

--- atividade2/main.cpp
+++ atividade2/main.cpp
@@ -5,67 +5,86 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	srand(time(NULL));
-
-	//Variables
+	//Variáveis
 	bool exit_program = false;
+	ObjectDetector objectDetector(0.8);
+	omp_lock_t framelock;
+	omp_lock_t namelock;
 	Mat frame(100, 100, CV_8UC3, Scalar(255, 0, 255));
-	ObjectDetector objectDetector;
-	VideoCapture cap(0); //default camera
-	omp_lock_t writelock;
-	omp_init_lock(&writelock);
+	string object_name("Object Not found");
+	VideoCapture cap(0);
 
+	//Se não conseguir abrir a câmera encerra
 	if(!cap.isOpened())
         return -1;
+    
+    //Inicializa locks    
+    omp_init_lock(&framelock);
+	omp_init_lock(&namelock);
 
+	//Janela de exibição de frames
 	namedWindow("Object Detector", WINDOW_AUTOSIZE);
-	namedWindow("Detection", WINDOW_AUTOSIZE);
 
-	//Do training of the database
-	objectDetector.Train();
-
-	//---------------------------------------------------------
-
+	//Inicio do programa
 	#pragma omp parallel sections
    	{ 
+   		/*
+   			Esta thread captura os frames da câmera e os exibe 
+   			na janela do programa.
+   		*/
      	#pragma omp section
      	{ 
 			while(!exit_program)
 			{
-				//Get next frame from camera
-				omp_set_lock(&writelock);
+				//Pega próximo frame da câmera e faz cópia para exibição
+				omp_set_lock(&framelock);
         		cap >> frame;
-				omp_unset_lock(&writelock); 
+        		Mat frame_show = frame;
+				omp_unset_lock(&framelock); 
+				
+				//Desenha nome do objeto na imagem do frame
+				omp_set_lock(&namelock);
+				putText(frame_show, object_name, Point(30, 30), FONT_ITALIC, 1, Scalar(0, 0, 0), 3, false);
+				putText(frame_show, object_name, Point(32, 32), FONT_ITALIC, 1, Scalar(0, 255, 255), 3, false);
+				omp_unset_lock(&namelock);
 
-				//Show frame on window
-				imshow("Object Detector", frame);
+				//Mostra o frame na janela
+				imshow("Object Detector", frame_show);
 
-				//Exit if any key pressed
+				//Sai do programa se alguma tecla for pressionada
 				if(waitKey(30) >= 0)
 					exit_program = true;
 			}
 		}
 
+		/*
+   			Esta thread processa os frames capturados e busca
+   			os objetos na imagem.
+   		*/
      	#pragma omp section
      	{ 
 			while(!exit_program)
 			{
-				//Convert frame to black and white
+				//Converte frame para preto e branco
+				omp_set_lock(&framelock);
 				Mat frame_bw;
-
-				omp_set_lock(&writelock);
 				cvtColor(frame, frame_bw, CV_BGR2GRAY);
-				omp_unset_lock(&writelock); 
+				omp_unset_lock(&framelock); 
 
-				//Process frame and show detected object in another window 
-				imshow("Detection", objectDetector.Detect(frame_bw));
-
-				cvWaitKey(10);
+				//Processa o frame e pega o nome do objeto encontrado
+				string detector_name = objectDetector.Detect(frame_bw);
+				
+				//Copia a string do nome para a string de exibicao
+				omp_set_lock(&namelock);
+				object_name = detector_name;
+				omp_unset_lock(&namelock);
 			}
 		}
    	}
 
-	omp_destroy_lock(&writelock);
+	//Libera os locks
+	omp_destroy_lock(&framelock);
+	omp_destroy_lock(&namelock);
     
 	return 0;
 }
--- atividade2/object_detector.h
+++ atividade2/object_detector.h
@@ -6,28 +6,57 @@
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/nonfree/features2d.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/ml/ml.hpp>
 
 #include <iostream>
 #include <vector>
 #include <cstdlib>
 #include <cstdio>
 #include <ctime>
+#include <string>
 #include <omp.h>
 
 using namespace std;
 using namespace cv;
 
-#define NUM_IMAGES 15
+class Object
+{
+	private:
+		int id;
+		string name;
+		vector<string> image_filenames;
+		
+	public:
+		Object() { }
+		Object(int id, string name, vector<string> image_filenames) { this->id = id; this->name = name; this->image_filenames = image_filenames; } 
+		void SetID(int id) { this->id = id; }
+		void SetName(string name) { this->name = name; }
+		void InsertImageFilename(string image_filename) { image_filenames.push_back(image_filename); }
+		
+		int GetID() { return id; }
+		string GetName() { return name; }
+		vector<string> GetFilenames() { return image_filenames; }
+};
 
 class ObjectDetector
 {
 	private:
-				
+		vector<Object> objects;
+		Mat dictionary;
+		Mat labels;
+		CvSVM svm;
+		
+		double confidence_threshold;
+		
+		void Init(double confidence);
+		void Train();
+		void LoadObjects();
+		Mat ComputeHistogram(Mat image);	
 
 	public:
-		ObjectDetector();
-		void Train();
-		cv::Mat Detect(cv::Mat frame);
+		ObjectDetector() { Init(0.8); }
+		ObjectDetector(double confidence_threshold) { Init(confidence_threshold); }
+		string Detect(Mat frame);
 };
 
 #endif
